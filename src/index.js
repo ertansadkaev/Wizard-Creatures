310827856a96dd32532c2c7f4d357e474938c55b
Ertan Sadkaev <125732019+ertansadkaev@users.noreply.github.com>
2024-02-08 23:34:30 +0200

    Handlebars configuration

--- src/index.js
+++ src/index.js
@@ -8,3 +8,8 @@ const app = express();
 //Express Configuration
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.resolve(__dirname, "../public")));
+
+//Handlebars Configuration
+app.engine("hbs", handlebars.engine({extname: "hbs"}));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
