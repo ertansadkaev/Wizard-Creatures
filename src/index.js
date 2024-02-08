582540bdc38627daa26ddd265443b1a18bedecd3
Ertan Sadkaev <125732019+ertansadkaev@users.noreply.github.com>
2024-02-08 23:38:18 +0200

    Add listening port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
+const { PORT } = require("./constants");
 
 // Local Variabels
 const app = express();
@@ -13,3 +14,5 @@ app.use(express.static(path.resolve(__dirname, "../public")));
 app.engine("hbs", handlebars.engine({extname: "hbs"}));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
