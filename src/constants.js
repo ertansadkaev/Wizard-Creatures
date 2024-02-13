2fe0c493eeecaf8dfa837a7e608bc8508f24bba1
Ertan Sadkaev <125732019+ertansadkaev@users.noreply.github.com>
2024-02-13 11:58:25 +0200

    connect DB

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const constants = {
     PORT: 3030,
+    DB_URL: 'mongodb://localhost:27017/Wizard-Creature',
 }
 
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
-const { PORT } = require("./constants");
+const { PORT, DB_URL } = require("./constants");
 const router = require("./routes");
+const mongoose = require("mongoose");
+const { error } = require("console");
 
 // Local Variabels
 const app = express();
@@ -15,5 +17,15 @@ app.use(express.static(path.resolve(__dirname, "../public")));
 app.engine("hbs", handlebars.engine({extname: "hbs"}));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+//Data base configuration
+async function dbConnect() {
+    await mongoose.connect(DB_URL);
+}
+dbConnect()
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch (error => console.log(`Error while connecting Database, Error: ${error}`));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
